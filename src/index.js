e1be3bbeb1727e07d62b6d173e07a66d8417e828
kovacsborka <kovacsborbalaterez@gmail.com>
2022-04-11 16:47:44 +0200

    header component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Beers from './components/Beers'
 import data from './data.json'
+import Header from './components/Header'
 
 // console.log(data.id);
 
@@ -9,6 +10,7 @@ import data from './data.json'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header title={data.logo} />
     <Beers list={data.cards} />
   </React.StrictMode>,
   document.getElementById('root')
